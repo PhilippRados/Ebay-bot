5fda89c8190480c939ab4d1f1eea53e13e6e1da4
Philipp <phil.black@gmx.net>
2020-05-03 21:41:00 +0200

    Added OPP and sending Email

--- ebay_bot.py
+++ ebay_bot.py
@@ -1,19 +1,57 @@
 import requests
-
-keyword = "canon pixma pro 10-s"
-get_price = requests.get(
-    f"https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SECURITY-APPNAME=PhilippR-sellinga-PRD-3bcdef085-e041a9f7&RESPONSE-DATA-FORMAT=json&GLOBAL-ID=EBAY-DE&keywords={keyword}&REST-PAYLOAD&paginationInput.entriesPerPage=5").json()
-
-price = get_price[
-    "findItemsByKeywordsResponse"][0]["searchResult"][0]["item"][1]["sellingStatus"][0]["currentPrice"][0]["__value__"]
-
-entry_count = get_price[
-    "findItemsByKeywordsResponse"][0]["searchResult"][0]["@count"]
+from testing_api import username, pw, receiver_ebay
+import smtplib
 
 
-print(f"The Top 5 {keyword}'s:")
+class Search_Ebay:
+    def __init__(self, keyword):
+        self.keyword = keyword
+        self.get_price = requests.get(
+            f"https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SECURITY-APPNAME=PhilippR-sellinga-PRD-3bcdef085-e041a9f7&RESPONSE-DATA-FORMAT=json&GLOBAL-ID=EBAY-DE&keywords={self.keyword}&REST-PAYLOAD&paginationInput.entriesPerPage=5").json()
+        self.entry_count = self.get_price[
+            "findItemsByKeywordsResponse"][0]["searchResult"][0]["@count"]
 
-for i in range(int(entry_count)):
-    print(i,
-          get_price["findItemsByKeywordsResponse"][0]["searchResult"][0]["item"][i]["title"][0]+":",
-          get_price["findItemsByKeywordsResponse"][0]["searchResult"][0]["item"][i]["sellingStatus"][0]["currentPrice"][0]["__value__"])
+    def getting_top_5(self):
+        # price = get_price[
+        #     "findItemsByKeywordsResponse"][0]["searchResult"][0]["item"][1]["sellingStatus"][0]["currentPrice"][0]["__value__"]
+
+        self.entry_count = self.get_price[
+            "findItemsByKeywordsResponse"][0]["searchResult"][0]["@count"]
+
+        print(f"The Top 5 {self.keyword}'s:")
+
+        for i in range(int(self.entry_count)):
+            print(i,
+                  self.get_price["findItemsByKeywordsResponse"][0]["searchResult"][0]["item"][i]["title"][0]+":",
+                  self.get_price["findItemsByKeywordsResponse"][0]["searchResult"][0]["item"][i]["sellingStatus"][0]["currentPrice"][0]["__value__"])
+
+    def send_mail(self, sender, receiver, threshhold):
+        list_of_prices = []
+
+        for i in range(int(self.entry_count)):
+            list_of_prices.append(self.get_price["findItemsByKeywordsResponse"][0]["searchResult"]
+                                  [0]["item"][i]["sellingStatus"][0]["currentPrice"][0]["__value__"])
+
+        for i, price in enumerate(list_of_prices):
+            if float(price) <= threshhold:
+                print(
+                    f'We have found a match: {self.get_price["findItemsByKeywordsResponse"][0]["searchResult"][0]["item"][i]["viewItemURL"][0]}')
+                # Logic for sending Email
+
+                server = smtplib.SMTP("smtp.gmail.com", port=587)
+                server.ehlo_or_helo_if_needed()
+                server.starttls()
+                server.ehlo_or_helo_if_needed()
+
+                server.login(sender, pw)
+                print(f"login worked")
+
+                subject = "Ebay-Suche erfolgreich"
+
+                message = f'Subject:{subject}\n\nDer {self.keyword} kostet nur {float(price)}€. Hier ist der Link:\n{self.get_price["findItemsByKeywordsResponse"][0]["searchResult"][0]["item"][i]["viewItemURL"][0]}'
+                server.sendmail(sender, receiver, message)
+                server.quit()
+
+
+drucker = Search_Ebay("canon pixma pro 10-s")
+drucker.send_mail(username, receiver_ebay, 400)
---
+++ testing_api.py
@@ -0,0 +1,3 @@
+username = "phil.rados@gmail.com"
+pw = "Q*wertmo#derat220801:"
+receiver_ebay = "frontline11475@gmail.com"
